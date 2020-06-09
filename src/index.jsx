d579f9d089862be46b09d0bde421d60fdcf3dbe3
jackrein <jreinagel4@gmail.com>
2020-06-08 19:56:36 -0600

    Make About work as a toggle button to show blurb

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import Form from 'react-bootstrap/Form';
 import ControlledCarousel from './components/Carousel.jsx';
 import './custom.scss';
 
@@ -10,8 +11,11 @@ class App extends React.Component {
     super(props);
     this.state = {
       photos: [],
-      currentPic: {}
+      currentPic: {},
+      show: false
     }
+
+    this.aboutMe = this.aboutMe.bind(this);
   }
 
   componentDidMount() {
@@ -22,20 +26,23 @@ class App extends React.Component {
       })
   }
 
+  aboutMe() {
+    this.setState({ show: !this.state.show })
+  };
+
   render () {
-
-
     return (
       <div className="app">
         <Navbar bg="dark" variant="dark">
           <Navbar.Brand>Journey to Myself</Navbar.Brand>
           <Nav className="mr-auto">
-            <Navbar.Text className="pull-right" href="#features">About</Navbar.Text>
+            <Navbar.Text className="pull-right" onClick={this.aboutMe}>About</Navbar.Text>
           </Nav>
         </Navbar>
+        <p id="blurb" style={{ display: this.state.show ? "block" : "none" }}>This project is a reflection on my physical transition. It covers my first six years on T. Click through to see how I've changed!</p>
         <ControlledCarousel photos={this.state.photos} />
         <div className="slidecontainer">
-          <input type="range" min="0" max="70" defaultValue="0" className="slider" id="slider" />
+          <Form.Control type="range" min="0" max="70" defaultValue="0" className="slider" id="slider" />
           <p>Months on T: <span id="label">{this.state.currentPic.t_time}</span></p>
         </div>
         <footer>
